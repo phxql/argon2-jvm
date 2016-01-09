f7169fedf18bee3fb6ca8414da72801ac9ee0acf
Moritz Kammerer <kammerer.moritz@gmail.com>
2016-01-09 20:40:42 +0100

    Documentation for Size_t and Uint32_t.

--- src/main/java/de/mkammerer/argon2/jna/Size_t.java
+++ src/main/java/de/mkammerer/argon2/jna/Size_t.java
@@ -3,11 +3,22 @@ package de.mkammerer.argon2.jna;
 import com.sun.jna.IntegerType;
 import com.sun.jna.Native;
 
+/**
+ * size_t type for C interaction.
+ */
 public class Size_t extends IntegerType {
+    /**
+     * Constructor.
+     */
     public Size_t() {
         this(0);
     }
 
+    /**
+     * Constructor.
+     *
+     * @param value Value.
+     */
     public Size_t(long value) {
         super(Native.SIZE_T_SIZE, value);
     }
--- src/main/java/de/mkammerer/argon2/jna/Uint32_t.java
+++ src/main/java/de/mkammerer/argon2/jna/Uint32_t.java
@@ -2,11 +2,22 @@ package de.mkammerer.argon2.jna;
 
 import com.sun.jna.IntegerType;
 
+/**
+ * uint32_t type for C interaction.
+ */
 public class Uint32_t extends IntegerType {
+    /**
+     * Constructor.
+     */
     public Uint32_t() {
         this(0);
     }
 
+    /**
+     * Constructor.
+     *
+     * @param value Value.
+     */
     public Uint32_t(int value) {
         super(4, value, true);
     }
