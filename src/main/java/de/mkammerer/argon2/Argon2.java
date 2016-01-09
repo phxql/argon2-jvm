06b89c3ce1349b82750cf144b9513bdbf3bc0ec0
Moritz Kammerer <kammerer.moritz@gmail.com>
2016-01-09 20:40:49 +0100

    Documentation for Argon2.

--- src/main/java/de/mkammerer/argon2/Argon2.java
+++ src/main/java/de/mkammerer/argon2/Argon2.java
@@ -1,9 +1,26 @@
 package de.mkammerer.argon2;
 
+/**
+ * Argon2 password hashing function.
+ */
 public interface Argon2 {
-    // String hashEncoded(int iterations, int memory, int parallelism, byte[] password, byte[] salt, int hashLength);
-
+    /**
+     * Hashes a password.
+     *
+     * @param iterations  Number of iterations
+     * @param memory      Sets memory usage to x kibibytes
+     * @param parallelism Number of threads and compute lanes
+     * @param password    Password to hash
+     * @return Hashed password.
+     */
     String hash(int iterations, int memory, int parallelism, String password);
 
+    /**
+     * Verifies a password against a hash.
+     *
+     * @param hash Hash.
+     * @param password Password.
+     * @return True if the password matches the hash, false otherwise.
+     */
     boolean verify(String hash, String password);
 }
