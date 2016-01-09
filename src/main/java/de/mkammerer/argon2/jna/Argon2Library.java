79b1ed3381c8126ab54d25f5ffd862631c0d54b5
Moritz Kammerer <kammerer.moritz@gmail.com>
2016-01-09 20:38:07 +0100

    Documentation for Argon2Library.

--- src/main/java/de/mkammerer/argon2/jna/Argon2Library.java
+++ src/main/java/de/mkammerer/argon2/jna/Argon2Library.java
@@ -3,9 +3,18 @@ package de.mkammerer.argon2.jna;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 
+/**
+ * JNA bindings for Argon2.
+ */
 public interface Argon2Library extends Library {
+    /**
+     * Singleton instance.
+     */
     Argon2Library INSTANCE = (Argon2Library) Native.loadLibrary("argon2", Argon2Library.class);
 
+    /**
+     * Return code if everything is okay.
+     */
     int ARGON2_OK = 0;
 
     /*
@@ -15,8 +24,32 @@ public interface Argon2Library extends Library {
                          const size_t saltlen, const size_t hashlen,
                          char *encoded, const size_t encodedlen);
      */
+
+    /**
+     * Hashes a password with Argon2i, producing an encoded hash.
+     *
+     * @param t_cost      Number of iterations
+     * @param m_cost      Sets memory usage to m_cost kibibytes
+     * @param parallelism Number of threads and compute lanes
+     * @param pwd         Pointer to password
+     * @param pwdlen      Password size in bytes
+     * @param salt        Pointer to salt
+     * @param saltlen     Salt size in bytes
+     * @param hashlen     Desired length of the hash in bytes
+     * @param encoded     Buffer where to write the encoded hash
+     * @param encodedlen  Size of the buffer (thus max size of the encoded hash)
+     * @return ARGON2_OK if successful
+     */
     int argon2i_hash_encoded(Uint32_t t_cost, Uint32_t m_cost, Uint32_t parallelism, byte[] pwd, Size_t pwdlen, byte[] salt, Size_t saltlen, Size_t hashlen, byte[] encoded, Size_t encodedlen);
 
+    /**
+     * Verifies a password against an encoded string
+     *
+     * @param encoded String encoding parameters, salt, hash
+     * @param pwd Pointer to password
+     * @param pwdlen Password size in bytes
+     * @return ARGON2_OK if successful
+     */
     /*
     int argon2i_verify(const char *encoded, const void *pwd, const size_t pwdlen);
      */
