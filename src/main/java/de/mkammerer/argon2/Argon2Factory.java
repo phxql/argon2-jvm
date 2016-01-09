32ee03bb04ba20f4b64ff358fa24b47712b3efdd
Moritz Kammerer <kammerer.moritz@gmail.com>
2016-01-09 20:41:37 +0100

    Documentation for Argon2Factory.

--- src/main/java/de/mkammerer/argon2/Argon2Factory.java
+++ src/main/java/de/mkammerer/argon2/Argon2Factory.java
@@ -1,9 +1,20 @@
 package de.mkammerer.argon2;
 
+/**
+ * Factory for {@link Argon2} instances.
+ */
 public final class Argon2Factory {
+    /**
+     * Static class, no instances allowed.
+     */
     private Argon2Factory() {
     }
 
+    /**
+     * Creates a new {@link Argon2} instance.
+     *
+     * @return Argon2 instance.
+     */
     public static Argon2 create() {
         return new Argon2i();
     }
