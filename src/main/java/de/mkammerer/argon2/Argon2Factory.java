fe25a698a60c815c04e06d262ffef0c1c45ad4b2
Moritz Kammerer <kammerer.moritz@gmail.com>
2016-04-11 22:47:56 +0200

    Added JavaDoc.

--- src/main/java/de/mkammerer/argon2/Argon2Factory.java
+++ src/main/java/de/mkammerer/argon2/Argon2Factory.java
@@ -10,9 +10,18 @@ public final class Argon2Factory {
     private Argon2Factory() {
     }
 
+    /**
+     * Argon2 type.
+     */
     public enum Argon2Types {
-      ARGON2i,
-      ARGON2d
+        /**
+         * Argon2i.
+         */
+        ARGON2i,
+        /**
+         * Argon2d.
+         */
+        ARGON2d
     }
 
     /**
@@ -24,14 +33,20 @@ public final class Argon2Factory {
         return create(Argon2Types.ARGON2i);
     }
 
+    /**
+     * Creates a new {@link Argon2} instance with the given type.
+     *
+     * @param type Argon2 type.
+     * @return Argon2 instance.
+     */
     public static Argon2 create(Argon2Types type) {
-      switch (type) {
-        case ARGON2i:
-          return new Argon2i();
-        case ARGON2d:
-          return new Argon2d();
-        default:
-          throw new IllegalArgumentException("Invalid argon2 type");
-      }
+        switch (type) {
+            case ARGON2i:
+                return new Argon2i();
+            case ARGON2d:
+                return new Argon2d();
+            default:
+                throw new IllegalArgumentException("Invalid argon2 type");
+        }
     }
 }
