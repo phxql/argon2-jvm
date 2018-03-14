60c09094e76a8c13a6740964c9d9dddc50a7347a
Moritz Kammerer <kammerer.moritz@gmail.com>
2018-03-14 19:39:42 +0100

    Make the warmup in Argon2Helper faster

--- src/main/java/de/mkammerer/argon2/Argon2Helper.java
+++ src/main/java/de/mkammerer/argon2/Argon2Helper.java
@@ -12,6 +12,18 @@ public final class Argon2Helper {
      * Number of warmup runs.
      */
     private static final int WARMUP_RUNS = 10;
+    /**
+     * Minimum number of iterations.
+     */
+    private static final int MIN_ITERATIONS = 1;
+    /**
+     * Minimum number of memory.
+     */
+    private static final int MIN_MEMORY = 8;
+    /**
+     * Minimm number of parallelism.
+     */
+    private static final int MIN_PARALLELISM = 1;
 
     /**
      * No instances allowed.
@@ -45,10 +57,7 @@ public final class Argon2Helper {
     public static int findIterations(Argon2 argon2, long maxMillisecs, int memory, int parallelism, IterationLogger logger) {
         char[] password = "password".toCharArray();
 
-        // Do some warmup runs for the JIT to kick in
-        for (int i = 0; i < WARMUP_RUNS; i++) {
-            argon2.hash(1, memory, parallelism, password);
-        }
+        warmup(argon2, password);
 
         long took;
         int iterations = 0;
@@ -63,6 +72,18 @@ public final class Argon2Helper {
         } while (took <= maxMillisecs);
 
         return iterations - 1;
+    }
+
+    /**
+     * Calls Argon2 a number of times to warm up the JIT
+     *
+     * @param argon2   Argon2 instance.
+     * @param password Some password.
+     */
+    private static void warmup(Argon2 argon2, char[] password) {
+        for (int i = 0; i < WARMUP_RUNS; i++) {
+            argon2.hash(MIN_ITERATIONS, MIN_MEMORY, MIN_PARALLELISM, password);
+        }
     }
 
     /**
